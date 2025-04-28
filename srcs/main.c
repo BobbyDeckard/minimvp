1d3decd3484243604469812b1c7b34dd9084bae4
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-04-28 13:59:34 +0200

    Removed unnecessary lines

--- srcs/main.c
+++ srcs/main.c
@@ -33,38 +33,3 @@ int main(int ac, char **av, char **envp)
 	cleanup(ast);
 	return (0);
 }
-
-// Fonction main appelant des fonctions chatgptisées pour simuler
-// un parsing.
-// Notre programme aura sans doute un main suivant cette forme générale.
-/*
-t_ast	*make_ast_from_input(const char *input);
-
-int main(int ac, char **av, char **envp)
-{
-	t_ast	*ast;
-	char	**paths;
-	char	*command;
-	char	*cwd;
-
-	(void) ac;
-	(void) av;
-	paths = get_paths();
-	while (1)
-	{
-		cwd = make_cwd();
-		command = readline(cwd);
-		free(cwd);
-		if (command)
-		{
-			add_history(command);
-			ast = make_ast_from_input(command);
-			exec_ast(ast, paths, envp);
-			free(command);
-		}
-		else
-			break;
-	}
-	return (0);
-}
-*/
