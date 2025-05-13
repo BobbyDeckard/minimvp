8101eebe32ef8179307ce76eb660d94a994fdd27
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-05-13 15:28:36 +0200

    Added comment

--- srcs/utils/ast_utils.c
+++ srcs/utils/ast_utils.c
@@ -6,12 +6,13 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:25:44 by imeulema          #+#    #+#             */
-/*   Updated: 2025/05/13 15:26:06 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/05/13 15:28:28 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
+// recursively sets the root pointer of each node to the tree's root node
 void	set_root_node(t_ast *ast, t_ast *root)
 {
 	int	i;
