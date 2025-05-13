0632935110c244712ad09faab424173077c7869f
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-05-13 16:12:26 +0200

    Normalized and spread functions

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,24 +6,11 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:37:39 by imeulema          #+#    #+#             */
-/*   Updated: 2025/05/13 16:06:22 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/05/13 16:10:51 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
-
-char	*get_str(const char *name)
-{
-	char	*str;
-
-	str = getenv(name);
-	if (!str)
-	{
-		perror(name);
-		return (NULL);
-	}
-	return (str);
-}
 
 void	print_variable(t_ast *node, const char *name)
 {
